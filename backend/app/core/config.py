778a91b68a5f31c0b89f3e0b0ba9c013e67ed41d
Pavitra Bhatt <pavitra.bhatt@kombee.com>
2025-06-14 17:35:57 +0530

    change in gitignore and import change

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
-from pydantic import BaseSettings, AnyHttpUrl
+from pydantic_settings import BaseSettings
+from pydantic import AnyHttpUrl
 import secrets
 import os
 
